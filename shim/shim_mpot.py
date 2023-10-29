30e7b6b0d6eb5f88d2810568e345b5f984eceafb
Asad Jeewa <asad.jeewa@gmail.com>
2023-10-29 02:09:44 +0200

    feat: fix load weights

--- shim/shim_mpot.py
+++ shim/shim_mpot.py
@@ -1,6 +1,6 @@
 import torch
 import numpy as np
-from shim.cleanrl_mappo_shared import Agent, batchify_obs
+from shim.cleanrl_mappo_shared import MAPPO, batchify_obs
 from supersuit import color_reduction_v0, frame_stack_v1, resize_v1
 
 #DEBUG START
@@ -24,32 +24,51 @@ else:
 
 observations = env.reset()[0]#wrapped in tuple
 steps = 0
-r = [0,0]
+r = []
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+if load_model:
+        # exp_name = "gpu_ctde_state_4x4__clean_up_simple__1__1695335842"
+        # exp_name = "gpu_ctde_state_4x4_divergent__clean_up_simple__1__1695350009"
+
+        exp_name = "sanity_single__clean_up_simple__3__1698623132"
+        # exp_name = "noturn_gpu_ctde_state_multicoord__clean_up_simple__1__1697719103"
+
+        modeldir = "model/TO PRESENT/"+exp_name
+        mappo = MAPPO(env.action_space(env.possible_agents[0]).n)
+        print(env.action_space(env.possible_agents[0]).n)
+        # exit()
+        # print("INIT")
+        # for param in mappo.actor.parameters():
+        #     print(param.data)
+        # print("LOAD WEIGHTS")
+        mappo.actor.load_state_dict(torch.load(modeldir,  map_location=torch.device('cpu')))
+        for param in mappo.actor.parameters():
+            print(param.data)
+        # exit()
+        # mappo.actor.eval()
+
 while env.agents:
-    
     if load_model:
-        # exp_name = "ctde_state_4x4_divergent__clean_up_simple__1__20-09-2023_14-51-32"
-        exp_name = "ctde_state_4x4__clean_up_simple__1__20-09-2023_14-51-30"
-        # exp_name = "gpu_ctde_state_4x4__clean_up_simple__1__20-09-2023_21-46-14"
-        # exp_name = "ctde_pistonball__clean_up_simple__1__20-09-2023_16-04-10"
-        modeldir = "model/current/"+exp_name
-        agent = Agent(env.action_space(env.possible_agents[0]).n)
-        model = agent.actor
-        model.load_state_dict(torch.load(modeldir,  map_location=torch.device('cpu')))
-        model.eval()
         obs = batchify_obs(observations,"cpu")
-        actions, logprobs, _, values = agent.get_actions_and_values(obs,len(env.possible_agents))
+        actions, logprobs, _, values = mappo.get_actions_and_values(obs,len(env.possible_agents))
         actions = dict(zip(env.agents, actions))
     else: 
-        obs = batchify_obs(observations,"cpu")
         actions = {agent: env.action_space(agent).sample() for agent in env.agents}
     observations, rewards, terminations, truncations, infos = env.step(actions)
-    r[0]+= rewards["player_0"]
-    r[1]+= rewards["player_1"]
+    print("REWARDS: ",rewards)
+    i = 0
+    for agent in env.agents:
+        if (i>len(r)-1):
+            r.append(rewards[agent])
+        else:
+             r[i]+= rewards[agent]
+        i+=1
+    # r[0]+= rewards["player_0"]
+    # r[1]+= rewards["player_1"]
     steps+=1
-    print("Step: ",steps," | Rewards: ",r)
+    print("Step: ",steps," | Cumulative Rewards: ",r)    
+    # print("ACTIONS: ",actions)
     if True in terminations:
         print("DONE")
         break
