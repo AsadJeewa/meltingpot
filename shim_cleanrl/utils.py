82703925cb09b19d0fa2846944e62a9abb69d42d
Asad Jeewa <asad.jeewa@gmail.com>
2023-11-23 15:26:29 +0200

    feat: utils

---
+++ shim_cleanrl/utils.py
@@ -0,0 +1,25 @@
+import numpy as np
+import torch 
+
+def batchify_obs(obs, device):#stack agents
+    """Converts PZ style observations to batch of torch arrays."""
+    obs = np.stack([obs[a] for a in obs], axis=0)#store only values and ignore keys
+    # transpose to be (batch, channel, height, width)
+    obs = obs.transpose(0, -1, 1, 2)
+    # convert to torch
+    obs = torch.tensor(obs).to(device)
+    return obs
+
+def batchify(x, device):
+    """Converts PZ style returns to batch of torch arrays."""
+    # convert to list of np arrays
+    x = np.stack([x[a] for a in x], axis=0)
+    # convert to torch
+    x = torch.tensor(x).to(device)
+    return x
+
+def unbatchify(x, env):
+    """Converts np array to PZ style arguments."""
+    x = x.cpu().numpy()
+    x = {a: x[i] for i, a in enumerate(env.possible_agents)}
+    return x
