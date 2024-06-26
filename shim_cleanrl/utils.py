0e26e3cfa2d7dc380481d7a3e70d5ad460c3502e
Asad Jeewa <asad.jeewa@gmail.com>
2024-06-26 20:26:16 +0200

    chore: move make_env to utils

--- shim_cleanrl/utils.py
+++ shim_cleanrl/utils.py
@@ -1,10 +1,13 @@
 import numpy as np
 import torch 
+from supersuit import color_reduction_v0, frame_stack_v1, resize_v1
+from shimmy import MeltingPotCompatibilityV0
+from shimmy.utils.meltingpot import load_meltingpot
 
 def batchify_obs(obs, device):#stack agents
     """Converts PZ style observations to batch of torch arrays."""
     obs = np.stack([obs[a] for a in obs], axis=0)#store only values and ignore keys
-    # transpose to be (batch, channel, height, width)
+    # transpose to be (batch, channel, height, width) from  (batch, height, width, channel) where channel can be stacks 
     obs = obs.transpose(0, -1, 1, 2)
     # convert to torch
     obs = torch.tensor(obs).to(device)
@@ -28,3 +31,11 @@ def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
     torch.nn.init.orthogonal_(layer.weight, std)
     torch.nn.init.constant_(layer.bias, bias_const)
     return layer
+
+def make_env(env_id, frame_size, stack_size):
+    env = load_meltingpot(env_id)
+    env = MeltingPotCompatibilityV0(env, render_mode="None")
+    env = color_reduction_v0(env, 'full') # grayscale
+    env = resize_v1(env, frame_size, frame_size) # resize and stack images
+    env = frame_stack_v1(env, stack_size)
+    return env
